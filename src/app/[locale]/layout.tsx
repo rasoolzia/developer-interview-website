d236b05022e3431aa93ec5c7d761678eb7245579
Rasool <rasoolol.77@gmail.com>
2026-08-17 15:47:28 +0330

    refactor(UI): delete LocaleProvider and app/layout.tsx, update the [locale].layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,15 +1,24 @@
+import "./../globals.css";
+
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { getMessages, setRequestLocale } from "next-intl/server";
 
 import { routing } from "@/shared/config/i18n";
-import { LocaleProvider } from "@/shared/ui/locale-provider";
 import { Footer } from "@/widgets/footer";
 import { Header } from "@/widgets/header";
+
+import { ThemeProvider } from "../providers/theme-provider";
 
 type Props = {
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
+};
+
+export const metadata: Metadata = {
+  title: "Developer Interview Handbook",
+  description: "Open source developer interview questions handbook",
 };
 
 export function generateStaticParams() {
@@ -26,16 +35,19 @@ export default async function LocaleLayout({ children, params }: Props) {
   setRequestLocale(locale);
 
   const messages = await getMessages();
+  const direction = locale === "fa" ? "rtl" : "ltr";
 
   return (
-    <NextIntlClientProvider messages={messages}>
-      <LocaleProvider>
-        <Header />
-
-        <main>{children}</main>
-
-        <Footer />
-      </LocaleProvider>
-    </NextIntlClientProvider>
+    <html lang={locale} dir={direction} suppressHydrationWarning>
+      <body>
+        <ThemeProvider>
+          <NextIntlClientProvider messages={messages}>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+          </NextIntlClientProvider>
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
