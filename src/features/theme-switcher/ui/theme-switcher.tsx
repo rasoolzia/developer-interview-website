dec013decd6ed523b6426a7994dc8b73485ee9c7
Rasool <rasoolol.77@gmail.com>
2026-08-23 22:34:11 +0330

    feat(ui): add tooltip to theme-switcher and update messages for header and footer

--- src/features/theme-switcher/ui/theme-switcher.tsx
+++ src/features/theme-switcher/ui/theme-switcher.tsx
@@ -4,7 +4,7 @@ import { useTheme } from "@teispace/next-themes";
 import { type LucideIcon, MoonIcon, SunIcon, SunMoonIcon } from "lucide-react";
 
 import { DEFAULT_THEME, Theme, THEMES } from "@/shared/constants";
-import { useMounted } from "@/shared/hooks";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/shared/ui/shadcn";
 
 const ICONS: Record<Theme, LucideIcon> = {
   system: SunMoonIcon,
@@ -14,16 +14,20 @@ const ICONS: Record<Theme, LucideIcon> = {
 
 export function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
-  const mounted = useMounted();
 
   const nextTheme = THEMES[(theme as Theme) || DEFAULT_THEME];
   const Icon = ICONS[(theme as Theme) || DEFAULT_THEME];
 
-  if (!mounted) return null;
-
   return (
-    <button onClick={() => setTheme(nextTheme)}>
-      <Icon />
-    </button>
+    <Tooltip>
+      <TooltipTrigger
+        render={
+          <button onClick={() => setTheme(nextTheme)}>
+            <Icon />
+          </button>
+        }
+      />
+      <TooltipContent>{theme} mode</TooltipContent>
+    </Tooltip>
   );
 }
